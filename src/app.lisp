88eac40d5dca76710e6268e8a8082864088c5401
Liszt21 <1832666492@qq.com>
2021-04-22 00:23:34 +0800

    add apps ohmyzsh & lemacs

--- src/app.lisp
+++ src/app.lisp
@@ -16,7 +16,6 @@
                    :update ,update
                    :start ,start
                    :stop ,stop)))))
-  
 
 (defun dispatch (apps actions)
   (loop for name in apps
@@ -33,7 +32,6 @@
                   (pathname (concatenate 'string *home* (subseq string 1)))
                   string)))
     path))
-
 
 (defun remove-folder (path)
   (let ((folder (string->path path)))
@@ -76,8 +74,52 @@
             (remove-folder "@/app/nvm"))
   :cover t)
 
+;; #-os-windows
+(defapp ohmyzsh :install '(progn
+                       (print "Install ohmyzsh")
+                       (uiop:run-program (format nil "~A;~A;~A"
+                                                 "export ZSH=$ALIYA/app/ohmyzsh"
+                                                 "export RUNZSH=no"
+                                                 "sh -c \"$(curl -fsSL https://gitee.com/mirrors/oh-my-zsh/raw/master/tools/install.sh)\"")
+                                         :output *standard-output*
+                                         :error-output *standard-output*)
+                       (let ((profile (merge-pathnames ".zshrc" (user-homedir-pathname))))
+                         (when (uiop:directory-exists-p profile)
+                            (rename-file profile (merge-pathnames ".zshrc.bak" (user-homedir-pathname))))
+                         (with-open-file (out profile :direction :output :if-exists :supersede)
+                          (with-standard-io-syntax
+                            ;; TODO Optimise
+                            (princ (format nil "source ~A" (concatenate 'string *home* "/etc/entry")) out)
+                            )))
+                       (git-clone "https://github.com/reobin/typewritten.git" "@/app/ohmyzsh/themes/typewritten")
+                       (uiop:run-program (format nil "ln -s ~A ~A" (concatenate 'string
+                                                                                *home*
+                                                                                "/app/ohmyzsh/themes/typewritten/typewritten.zsh-theme")
+                                                                   (concatenate 'string
+                                                                                *home*
+                                                                                "/app/ohmyzsh/themes/typewritten.zsh-theme")))
+                       (git-clone "https://github.com/zsh-users/zsh-autosuggestions" "@/app/ohmyzsh/plugins/zsh-autosuggestions")
+                       (git-clone "https://github.com/zsh-users/zsh-syntax-highlighting" "@/app/ohmyzsh/plugins/zsh-syntax-highlighting")
+                        )
+  :update '(print "Update ohmyzsh")
+  :remove '(progn
+            (print "Remove ohmyzsh")
+            (remove-folder "@/app/ohmyzsh"))
+  :cover t)
+
+(defapp lemacs :install '(progn
+                       (print "Install lemacs")
+                       (git-clone "https://github.com/Liszt21/Lemacs" "@/app/lemacs"))
+  :update '(print "Update lemacs")
+  :remove '(progn
+            (print "Remove lemacs")
+            (remove-folder "@/app/lemacs"))
+  :cover t)
+
 (defun package-manager (action name &key scoop yay apt)
   (let ((mapper '(:yay (:install "-Ss" :remove "-Rcus")
                   :scoop (:install "install" :remove "uninstall")
                   :apt (:install "install" :remove "remove"))))
        (getf (getf mapper name) action)))
+
+       
